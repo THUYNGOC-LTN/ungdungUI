de8120882689e1af9e75ee602704a0479c0b0b83
THUYNGOC-LTN <lamngoc21082005@gmail.com>
2025-05-13 23:06:18 +0700

    Update ungdungUI.dart

--- ungdungUI.dart
+++ ungdungUI.dart
@@ -1,1 +1,45 @@
-# ungdungUI
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ProfilePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: NetworkImage(
+                  'https://randomuser.me/api/portraits/men/75.jpg'),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Johan Smith',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'California, USA',
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
